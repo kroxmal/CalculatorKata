c498d1408f52f5925ef8fcecb9efb6210b097e5a
kroxmal <iukrohmal@yandex.ru>
2022-11-18 11:03:30 +0300

    Calculator

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(calc(input));
+    }
     public static String calc(String input){
         String[] str = input.split(" ");
         if (str.length == 1){
